4bca77898f3daed1daa564689d3d0de5823f4e85
alarivas <alarivas@udec.cl>
2017-06-22 22:45:00 -0400

    fixed infoSenadores

--- infoSenadores.py
+++ infoSenadores.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 url = "http://www.senado.cl/appsenado/index.php?mo=senadores&ac=listado"
 
@@ -13,7 +14,18 @@ if status_code == 200:
 	senadores = html.find_all('tr')
 	partido = html.find_all('td')
 	for i in range(2,39*2,2):
-		print("Nombre: " + senadores[i].contents[3].contents[1].contents[0].getText()) 
-		print("Región: " + senadores[i].contents[3].contents[3].contents[1].getText().strip()+ '\n'+"Circunscripción: "+senadores[i].contents[3].contents[3].contents[3].getText())
-		print(senadores[i].contents[3].contents[5].contents[0].string  + senadores[i].contents[3].contents[5].contents[1].getText())
-		print(partido[2*i - 1].getText()+'\n')
+		aux = senadores[i].contents[3].contents[5].contents[0].string
+		tmp= re.split(' |', aux)
+
+		nombre = senadores[i].contents[3].contents[1].contents[0].getText()
+		region =senadores[i].contents[3].contents[3].contents[1].getText().strip()
+		circunscripcion = senadores[i].contents[3].contents[3].contents[3].getText()
+		telefono = tmp[1]+tmp[2]
+		mail = senadores[i].contents[3].contents[5].contents[1].getText()
+		part = re.split(' ',partido[2*i - 1].getText())
+		print("Nombre: " + nombre) 
+		print("Región: " + region + '\n'+"Circunscripción: "+circunscripcion)
+		print("Teléfono: " + telefono)
+		print("email: " + mail)
+		print("Partido: " + part[1] + '\n')
+		
